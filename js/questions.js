0b798d382af206328c7bf8ce4a8c0cb809d38697
Not Kijana <Cloudhashira@icloud.com>
2021-08-20 11:43:18 -0400

    Added Question Number and Image to Obj

--- js/questions.js
+++ js/questions.js
@@ -1,6 +1,8 @@
 var ShaqQuestions = [
     {
         question: 'Can Shaq make freethrows?',
+        questionNumber: '1',
+        image: '/images/question1.jpg',
         answers: {
             A: 'No',
             B: 'Yes',
