20a41ca7e1754ffe224ef01e71dbe1b134f31e18
Not Kijana <Cloudhashira@icloud.com>
2021-08-20 11:37:27 -0400

    Added Base HTML and Question Obj

---
+++ js/questions.js
@@ -0,0 +1,12 @@
+var ShaqQuestions = [
+    {
+        question: 'Can Shaq make freethrows?',
+        answers: {
+            A: 'No',
+            B: 'Yes',
+            C: 'Sometimes',
+            D: 'Never'
+        },
+        correctAnswer: 'D'
+    },
+]
